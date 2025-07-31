a2ec859e7c13380b2d9e295fdd739e5c7fe9fb81
RM92023 <robinjara20@gmail.com>
2025-07-31 03:09:11 -0500

    feat(frontend): authentication services, guards, and token handling

---
+++ Frontend/src/main.ts
@@ -0,0 +1,13 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { JwtInterceptor } from './app/interceptors/jwt.interceptor';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([JwtInterceptor]))
+  ]
+}).catch(err => console.error(err));
